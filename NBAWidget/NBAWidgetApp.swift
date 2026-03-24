d1234eb3011ad681bde31a52ce33cc5dc63f453b
Shawn Dong <yanshengtian@gmail.com>
2026-03-24 11:27:29 -0400

    Initial Commit

---
+++ NBAWidget/NBAWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  NBAWidgetApp.swift
+//  NBAWidget
+//
+//  Created by dyst on 3/24/26.
+//
+
+import SwiftUI
+
+@main
+struct NBAWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
